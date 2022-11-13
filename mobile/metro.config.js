40f89394bbcff0a8661eeb12079d1f5a95f2ea0e
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-13 19:16:19 -0300

    fix: initial config

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,7 +3,24 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+ module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
       },
     }),
   },
